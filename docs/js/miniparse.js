adc95d5cdb782897aa77b9be8fa6a3b3ccfa4af1
Reffet <31766216+Reffet@users.noreply.github.com>
2019-11-17 12:34:50 +0900

    Update miniparse.js

--- docs/js/miniparse.js
+++ docs/js/miniparse.js
@@ -2,70 +2,70 @@ var vueData = null;
 
 function init() {
 
-//----------------------------------------------------------------------------------------
-// Vueオブジェクトの生成
-//----------------------------------------------------------------------------------------
-vueData = new Vue({
-	el : "body",
-	data : {
-		encounter : null,
-		combatant : null,
-		nodata    : nodata,
-		barGuide  : barGuide,
-		checkBox  : changeMode,
-		mode      : windowMode,
-		maxDps    : 0,
-		maxHps    : 0
-	},
-	attached : function() {
-		document.addEventListener("onOverlayDataUpdate", this.setTimer);
-		document.addEventListener("onOverlayStateUpdate", this.updateState);
-	},
-	detached : function() {
-		document.removeEventListener("onOverlayStateUpdate", this.updateState);
-		document.removeEventListener("onOverlayDataUpdate", this.setTimer);
-	},
-	methods : {
-		getBackClass     : getBackClass,
-		setTimer         : setTimer,
-		update           : update,
-		updateState      : updateState,
-		hideBody         : resetData,
-		changeJobToRole  : changeJobToRole,
-		hpsOn            : hpsOn,
-		damageOn         : damageOn,
-		getDpsBarWidth   : getDpsBarWidth,
-		getHpsBarWidth   : getHpsBarWidth,
-		isDpsBar         : isDpsBar,
-		isHpsBar         : isHpsBar,
-		getNameColor     : getNameColor,
-		getTohitColor    : getTohitColor,
-		getIconName      : jobIcon,
-		getCharName      : getDisplayName,
-		roundUp          : roundUp
-	}
-});
+	//----------------------------------------------------------------------------------------
+	// Vueオブジェクトの生成
+	//----------------------------------------------------------------------------------------
+	vueData = new Vue({
+		el : "body",
+		data : {
+			encounter : null,
+			combatant : null,
+			nodata    : nodata,
+			barGuide  : barGuide,
+			checkBox  : changeMode,
+			mode      : windowMode,
+			maxDps    : 0,
+			maxHps    : 0
+		},
+		attached : function() {
+			document.addEventListener("onOverlayDataUpdate", this.setTimer);
+			document.addEventListener("onOverlayStateUpdate", this.updateState);
+		},
+		detached : function() {
+			document.removeEventListener("onOverlayStateUpdate", this.updateState);
+			document.removeEventListener("onOverlayDataUpdate", this.setTimer);
+		},
+		methods : {
+			getBackClass     : getBackClass,
+			setTimer         : setTimer,
+			update           : update,
+			updateState      : updateState,
+			hideBody         : resetData,
+			changeJobToRole  : changeJobToRole,
+			hpsOn            : hpsOn,
+			damageOn         : damageOn,
+			getDpsBarWidth   : getDpsBarWidth,
+			getHpsBarWidth   : getHpsBarWidth,
+			isDpsBar         : isDpsBar,
+			isHpsBar         : isHpsBar,
+			getNameColor     : getNameColor,
+			getTohitColor    : getTohitColor,
+			getIconName      : jobIcon,
+			getCharName      : getDisplayName,
+			roundUp          : roundUp
+		}
+	});
 
-if (debug) {
-	// Dispatch OverlayDataUpdate Event
-	var debugEvent = document.createEvent("CustomEvent");
-	debugEvent.initCustomEvent("onOverlayDataUpdate", false, true, debugData);
-	document.dispatchEvent(debugEvent);
+	if (debug) {
+		// Dispatch OverlayDataUpdate Event
+		var debugEvent = document.createEvent("CustomEvent");
+		debugEvent.initCustomEvent("onOverlayDataUpdate", false, true, debugData);
+		document.dispatchEvent(debugEvent);
 
-	// 秒間隔でデータを切り替え
-	var cnt = 0;
-	if (loop > 0) {
-		setInterval(function() {
-			var cntVal = "";
-			if ((cnt % 2) == 1) {
-				cntVal = "2"
-			}
-			var evalData = eval("debugData" + cntVal);
-			debugEvent = document.createEvent("CustomEvent");
-			debugEvent.initCustomEvent("onOverlayDataUpdate", false, true, evalData);
-			document.dispatchEvent(debugEvent);
-			cnt++;
-		}, loop * 1000);
+		// 秒間隔でデータを切り替え
+		var cnt = 0;
+		if (loop > 0) {
+			setInterval(function() {
+				var cntVal = "";
+				if ((cnt % 2) == 1) {
+					cntVal = "2"
+				}
+				var evalData = eval("debugData" + cntVal);
+				debugEvent = document.createEvent("CustomEvent");
+				debugEvent.initCustomEvent("onOverlayDataUpdate", false, true, evalData);
+				document.dispatchEvent(debugEvent);
+				cnt++;
+			}, loop * 1000);
+		}
 	}
-}
 }
